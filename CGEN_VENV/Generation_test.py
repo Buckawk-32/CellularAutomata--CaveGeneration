fb2e0f7d8b9370935ef33885335ca2febad85af5
Nethika Jayith Rajapakse <117026619+Buckawk-32@users.noreply.github.com>
2025-01-29 00:29:46 +0800

    Added gitignore

--- CGEN_VENV/Generation_test.py
+++
@@ -1,5 +0,0 @@
-from Cave_Gen import CaveGeneration_CA
-
-CA = CaveGeneration_CA((1280, 720), None)
-
-CA.generate()
---
+++ Generation_Test.py
@@ -0,0 +1,5 @@
+from Cave_Gen import CaveGeneration_CA
+
+CA = CaveGeneration_CA((1920, 1080), None)
+
+CA.generate()
